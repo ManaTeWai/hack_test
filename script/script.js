35f3bd2b36ef59310c4ec1056be01f9e384e52bd
Максим Цыбульский <86505316+ManaTeWai@users.noreply.github.com>
2023-02-09 03:43:06 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,14 @@
+let el1 = document.getElementById('el1')
+let el2 = document.getElementById('el2')
+let el3 = document.getElementById('el3')
+let el4 = document.getElementById('el4')
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth <= 600) {
+      el2.classList.add('order-3');
+      el2.classList.remove('order-1');
+    } else {
+      el2.classList.remove('order-3');
+      el2.classList.add('order-1')
+    }
+});
